c9bb1ee8974c188f2c6a9f430fcf34761a7364eb
UpCoTe <msMulti@outlook.com>
2015-07-21 18:08:16 +0800

    3
    继续测试

--- NoLogin.py
+++ NoLogin.py
@@ -4,8 +4,19 @@ __author__ = 'jincj'
 #参考网址
 #http://blog.csdn.net/beiji_nanji/article/details/7487472
 #http://cuiqingcai.com/990.html
+#http://www.qiushibaike.com/8hr/page/2?s=4791266
 
 import urllib.request
+import re
+
+#存储文件
+def saveFile(data, fileName):
+    save_Path = 'E:\\Python\\SaveFile\\'
+    save_Path += fileName
+    print(save_Path)
+    f_obj = open(save_Path, 'wb')
+    f_obj.write(data)
+    f_obj.close()
 
 class NoLoginRequest:
     def __init__(self,url,page):
@@ -23,6 +34,7 @@ class NoLoginRequest:
         self._header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',}
 
 
+
     def start(self):
         #for key,value in self._header.items():
             #print(key,':',value)
@@ -30,11 +42,14 @@ class NoLoginRequest:
         try:
             for page in range(1,self._page+1):
                 requestUrl = self._url + str(page)
-                print('正在访问网页   '+requestUrl)
-                #req = urllib.request.Request(requestUrl, headers=self._header)
-                #response = urllib.request.urlopen(req)
-                #data = response.read().decode()
-                #print(data)
+                print('正在访问网页   ' + requestUrl)
+                req = urllib.request.Request(requestUrl, headers=self._header)
+                response = urllib.request.urlopen(req)
+                data = response.read()
+                saveFile(data, 'qiushibaike.html')
+                #print(data.decode())
+                data = data.decode()
+                pattern = re.compile()
 
         except urllib.error.URLError as e:
             if hasattr(e, 'reason'):
@@ -47,8 +62,7 @@ class NoLoginRequest:
 
 
 
-
-#http://www.qiushibaike.com/8hr/page/2?s=4791266
+#
 noLoginRequest = NoLoginRequest('http://www.qiushibaike.com/8hr/page/', 5)
 noLoginRequest.start()
 
